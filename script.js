05ee88fef741f07d023643145129e1292a03d68b
Ruchi Badkur <94377483+RuchiBadkur@users.noreply.github.com>
2024-03-24 23:12:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 function letterCombinations(input_digit) {
-    if (!input_digit) return [];
+  //Complete the function
+	if (!input_digit) return [];
 
-    const map = {
-        "2": "abc", "3": "def", "4": "ghi", "5": "jkl",
-        "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"
+    const digitToLetters = {
+        '2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl',
+        '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'
     };
 
     const result = [];
 
-    function generateCombination(index, current) {
+    function generateCombinations(index, currentCombination) {
         if (index === input_digit.length) {
-            result.push(current);
+            result.push(currentCombination);
             return;
         }
 
-        const letters = map[input_digit[index]];
+        const letters = digitToLetters[input_digit[index]];
         for (let i = 0; i < letters.length; i++) {
-            generateCombination(index + 1, current + letters[i]);
+            generateCombinations(index + 1, currentCombination + letters[i]);
         }
     }
 
-    generateCombination(0, "");
-	console.log(letterCombinations(input_digit));
+    generateCombinations(0, '');
+
     return result.sort();
 }
-
 
 module.exports = letterCombinations;
